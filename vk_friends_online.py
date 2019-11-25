c363d215d8e046beaf6499844c60fcb3284b0a1b
Rustymattok <you@example.com>
2019-11-25 23:13:15 +0300

    this is frist commitment

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,54 @@
+import argparse
 import vk
+from vk.exceptions import VkAuthError
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
-
-def get_user_login():
-    pass
-
-
-def get_user_password():
-    pass
+APP_ID = 7221330
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope='friends'
+        )
+        api_friends = vk.API(session, v='5.85')
+        friends_online = api_friends.friends.getOnline()
+        friends_online_info = api_friends.users.get(
+            user_ids=friends_online,
+            fields='nickname'
+        )
+    except VkAuthError:
+        return None
+    return friends_online_info
 
 
-def output_friends_to_console(friends_online):
-    pass
+def output_friends_to_console(friends_online_info):
+    if friends_online_info is not None:
+        for friend_name in friends_online_info:
+            print(friend_name['first_name'], friend_name['last_name'])
+
+
+def create_parser():
+    parser_arg = argparse.ArgumentParser()
+    parser_arg.add_argument('-l',
+                            '--login',
+                            required=True,
+                            help="command - input file"
+                            )
+    parser_arg.add_argument('-p',
+                            '--password',
+                            required=True,
+                            help="command - input file"
+                            )
+    return parser_arg
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    parser = create_parser()
+    args = parser.parse_args()
+    login_user = args.login
+    password_user = args.password
+    user_friends_online = get_online_friends(login_user, password_user)
+    output_friends_to_console(user_friends_online)
